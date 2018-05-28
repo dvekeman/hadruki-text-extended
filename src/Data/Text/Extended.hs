97a8b1bea0469c58a36bae1a6f5c8d70080bb4cb
dvekeman <dieter.vekeman@gmail.com>
2018-05-28 11:02:17 +0200

    Add trim

--- src/Data/Text/Extended.hs
+++ src/Data/Text/Extended.hs
@@ -1,9 +1,18 @@
 module Data.Text.Extended (
   module Data.Text
   , showInt
+  , trim
 ) where
 
 import           Data.Text
+import           Prelude hiding ( head, last, tail, init, null )
 
 showInt :: Int -> Text
 showInt = pack . show
+
+trim :: Text -> Text
+trim s
+  | null s        = s
+  | head s == ' ' = trim $ tail s
+  | last s == ' ' = trim $ init s
+  | otherwise       = s 
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Test.Hspec
+import qualified Data.Text.Extended as T
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Data.Text.Extended.trim" $ do
+    it "can trim empty strings" $
+      T.trim "" `shouldBe` ("" :: T.Text)
+    it "can trim non-empty strings with a leading spaces" $ 
+      T.trim " blah" `shouldBe` ("blah" :: T.Text)
+    it "can trim non-empty strings with a trailing spaces" $ 
+      T.trim "blah " `shouldBe` ("blah" :: T.Text)
+    it "can trim non-empty strings with leading spaces" $ 
+      T.trim "   blah" `shouldBe` ("blah" :: T.Text)
+    it "can trim non-empty strings with trailing spaces" $ 
+      T.trim "blah       " `shouldBe` ("blah" :: T.Text)
+    it "can trim non-empty strings without leading or trailing spaces" $ 
+      T.trim "blah" `shouldBe` ("blah" :: T.Text)
+    it "can trim non-empty strings without leading or trailing spaces but with spaces in between" $ 
+      T.trim "je m'appelle Claude" `shouldBe` ("je m'appelle Claude" :: T.Text)
+
+  describe "Data.Text.Extended.trim" $ do
+    it "can convert integers to text" $
+      T.showInt 42 `shouldBe` ("42" :: T.Text)
+    it "can convert negative integers to text" $
+      T.showInt (-42) `shouldBe` ("-42" :: T.Text)
