62c9dcd989b7282510d6bdd15d56ec45899884d5
dvekeman <dieter.vekeman@gmail.com>
2018-05-28 11:17:35 +0200

    Add readEither

--- src/Data/Text/Extended.hs
+++ src/Data/Text/Extended.hs
@@ -2,9 +2,11 @@ module Data.Text.Extended (
   module Data.Text
   , showInt
   , trim
+  , readEither
 ) where
 
 import           Data.Text
+import qualified Text.Read as TR
 import           Prelude hiding ( head, last, tail, init, null )
 
 showInt :: Int -> Text
@@ -16,3 +18,8 @@ trim s
   | head s == ' ' = trim $ tail s
   | last s == ' ' = trim $ init s
   | otherwise       = s 
+
+-- TODO: optimize
+readEither :: Text -> Text
+readEither = TR.readEither . T.pack 
+  
